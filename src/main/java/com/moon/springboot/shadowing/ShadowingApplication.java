4b715ebac599c09629cd91cfd434b70e3982aa2b
hsmoon <hsmoon@hsmoonui-MacBookPro.local>
2020-11-03 09:53:21 +0900

    Initial commit

---
+++ src/main/java/com/moon/springboot/shadowing/ShadowingApplication.java
@@ -0,0 +1,13 @@
+package com.moon.springboot.shadowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowingApplication {`
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShadowingApplication.class, args);
+    }
+
+}
