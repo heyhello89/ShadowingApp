e3a6830808d7051050f7ff648a26714a07134c79
hsmoon <heyhello89@naver.com>
2020-11-03 18:25:58 +0900

    Spring Boot start!

--- src/main/java/com/moon/springboot/shadowing/ShadowingApplication.java
+++ src/main/java/com/moon/springboot/shadowing/ShadowingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShadowingApplication {`
+public class ShadowingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShadowingApplication.class, args);
---
+++ src/main/java/com/moon/springboot/shadowing/test/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.moon.springboot.shadowing.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/home")
+    public String home() {
+
+        return "index.html";
+    }
+
+    @ResponseBody
+    @RequestMapping("/valueTest")
+    public String valueTest() {
+        String value = "테스트 Text";
+
+        return value;
+    }
+
+}
