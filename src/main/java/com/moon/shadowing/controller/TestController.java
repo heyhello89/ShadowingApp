92a348ca9ad67230f1e995b46b031a801012e937
hsmoon <heyhello89@naver.com>
2020-11-10 19:57:22 +0900

    MySQL 연동 및 domain, repository, service 설정

--- src/main/java/com/moon/shadowing/controller/TestController.java
+++ src/main/java/com/moon/shadowing/controller/TestController.java
@@ -1,20 +1,29 @@
 package com.moon.shadowing.controller;
 
 import com.moon.shadowing.domain.UserMaster;
+import com.moon.shadowing.repository.UserMasterRepository;
 import com.moon.shadowing.service.UserMasterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @Controller
 @SessionAttributes("userMaster")
 public class TestController {
 
     @Autowired
     private UserMasterService userMasterService;
+
+    @Autowired
+    private UserMasterRepository userMasterRepository;
 
     @ModelAttribute("userMaster")
     private UserMaster openapiMaster() {
@@ -27,24 +36,34 @@ public class TestController {
         return "index.html";
     }
 
-    // TODO: html과 mapping하여 데이터 저장할 수 있도록 구현
     @PostMapping("/")
-    public String home(Model model, HttpServletRequest request) {
+    public String home(Model model, @ModelAttribute("userMaster") UserMaster master, BindingResult bindingResult, HttpServletRequest request, SessionStatus sessionStatus, RedirectAttributes redirectAttributes) {
 
-        String id = request.getParameter("id");
-        String passwd = request.getParameter("passwd");
-        String username = request.getParameter("username");
-        String birthday = request.getParameter("birthday");
+//        String id = request.getParameter("id");
+//        String passwd = request.getParameter("passwd");
+//        String username = request.getParameter("username");
+//        String birthday = request.getParameter("birthday");
+//
+//        model.addAttribute("id", id);
+//        model.addAttribute("passwd", passwd);
+//        model.addAttribute("user_name", username);
+//        model.addAttribute("birthday", birthday);
 
-        model.addAttribute("id", id);
-        model.addAttribute("passwd", passwd);
-        model.addAttribute("user_name", username);
-        model.addAttribute("birthday", birthday);
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("errorMessage", "입력값을 확인해주세요.");
+            return "/";
+        }
 
         System.out.println(model.toString());
+        System.out.println(master.getId());
+
+        UserMaster saved = userMasterRepository.save(master);
+        log.info("[saved master] {}", saved.toString());
+        sessionStatus.setComplete();
+        redirectAttributes.addFlashAttribute("resultMessage", "등록되었습니다.");
 
         return "redirect:/";
     }
 
-    // TODO: User 정보를 리스트로 확인할 수 있도록 API 구
+    // TODO: User 정보를 리스트로 확인할 수 있도록 API 구현
 }
